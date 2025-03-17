37e6aa2170e1a89703293108a7a9f77efdb2c5e2
tyrone-yanaga <tkyanaga@gmail.com>
2025-03-16 21:55:58 -0700

    auth endpoints

---
+++ no_caps/api/v1/endpoints/auth.py
@@ -0,0 +1,28 @@
+# app/api/v1/endpoints/auth.py
+from datetime import timedelta
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
+from sqlalchemy.orm import Session
+from core.auth import create_access_token, ACCESS_TOKEN_EXPIRE_MINUTES, Token
+from db.session import get_db
+from db.crud.user import authenticate_user
+
+router = APIRouter()
+
+@router.post("/login", response_model=Token)
+async def login_for_access_token(
+    form_data: OAuth2PasswordRequestForm = Depends(),
+    db: Session = Depends(get_db)
+):
+    user = authenticate_user(db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect email or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.email}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
