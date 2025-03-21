7ce9f7d2257d23993d41636d24a38183ed495350
tyrone-yanaga <tkyanaga@gmail.com>
2025-03-21 00:06:45 -0700

    update to use auth.py functionality

--- no_caps/services/audio_service.py
+++ no_caps/services/audio_service.py
@@ -1,37 +1,12 @@
 # app/services/audio_service.py
 from fastapi import UploadFile, HTTPException, Depends, status
-from fastapi.security import OAuth2PasswordBearer
-from jose import JWTError, jwt
 from sqlalchemy.orm import Session
 from db.crud import audio as audio_crud
-from db.crud.user import get_user_by_id, User
-from no_caps.dependencies import get_db
 from utils.file_handling import save_upload_file
 from utils.audio_processing import get_audio_duration
+from core.auth import get_current_user
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/token")
-SECRET_KEY = "your_secret_key"  # Replace with your actual secret key
-ALGORITHM = "HS256"
-
-async def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)) -> User:
-    credentials_exception = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Could not validate credentials",
-        headers={"WWW-Authenticate": "Bearer"},
-    )
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        user_id: int = payload.get("sub")
-        if user_id is None:
-            raise credentials_exception
-    except JWTError:
-        raise credentials_exception
-    user = get_user_by_id(db, user_id)
-    if user is None:
-        raise credentials_exception
-    return user
-
-async def process_audio_upload(file: UploadFile, db: Session, current_user: User = Depends(get_current_user)):
+async def process_audio_upload(file: UploadFile, db: Session, current_user = Depends(get_current_user)):
     if not file.content_type.startswith('audio/'):
         print('FILE CONTENT TYPE:', file.content_type)
         raise HTTPException(
